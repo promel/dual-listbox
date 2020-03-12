00fa5ab017b23d7598e190ca59114f4314a462cf
Joseph More <josephm@ps.studio>
2020-03-12 12:23:12 +0200

    Rmoved unneccesary stuff

--- main.js
+++ main.js
@@ -1,20 +1,6 @@
 let selectedList = document.querySelector('#custom-columns-selected');
 let unselectedList = document.querySelector('#custom-columns-unselected');
 
-function attendanceRegister() {
-    let custom_columns = {}
-    for (let option of selectedList.options) {
-        custom_columns[option.text] = option.value;
-    }
-
-    if (Object.keys(custom_columns).length == 0) {
-       alert("Please selected atleast 1 field")
-        return;
-    }
-
-    console.log(custom_columns);
-}
-
 function addCustomField() {
     let option = document.createElement('option');
     let custom_field = document.querySelector("#custom-field").value
@@ -45,7 +31,6 @@ function moveToSelect(select,moveAll) {
             
             let x = fromSelect.options.length-1;
             for(;x>-1;x--){
-                console.log(x);
                 if(fromSelect.options[x].selected){
                     toSelect.add(fromSelect.options[x]);
                 }
@@ -62,7 +47,6 @@ function moveSelection(direction) {
     
     let action = actions[direction]
     for(let option of selectedList.options){
-
         if(!option[action] || !option.selected) continue;
         let temp = option.cloneNode(true); 
         option.value = option[action].value;
